9c9deb372ac5ee68b849d4c1a248818971109a30
Vaibhav Gupta <vaibhavgupta7625@gmail.com>
2025-11-29 22:48:31 +0530

    Updated flask_app.py for deployment

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 from gemini_core import process_user_input
+import os
 
 app = Flask(__name__)
 
@@ -83,4 +84,5 @@ def get_response():
 # -------------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
